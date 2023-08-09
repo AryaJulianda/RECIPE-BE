7602a3acecf323e7d0a296eff949ffb5ca877455
AryaJulianda <official.aryajulianda@gmail.com>
2023-08-09 12:01:25 +0700

    ubah env

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,18 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const dbUsername = process.env.DB_USERNAME;
-const dbPassword = process.env.DB_PASSWORD;
-const dbPort = process.env.DB_PORT;
-const dbHost = process.env.DB_HOST;
-const dbDatabase = process.env.DB_DATABASE;
+// const dbUsername = process.env.DB_USERNAME ;
+// const dbPassword = process.env.DB_PASSWORD;
+// const dbPort = process.env.DB_PORT;
+// const dbHost = process.env.DB_HOST;
+// const dbDatabase = process.env.DB_DATABASE;
+
+const dbUsername = process.DB_USERNAME;
+const dbPassword = process.DB_PASSWORD;
+const dbPort = process.DB_PORT;
+const dbHost = process.DB_HOST;
+const dbDatabase = process.DB_DATABASE;
+
 const pool = new Pool({
   user: dbUsername,
   host: dbHost,
@@ -17,6 +24,7 @@ const pool = new Pool({
 pool.connect((err) => {
   if (err) {
     console.error('Error connecting to the database:', err);
+    console.log(dbUsername,dbHost)
   } else {
     console.log('Connected to the database!');
   }
