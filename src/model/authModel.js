368631e1e783acd5c5dcdfae15f1aa9cab29ecb4
AryaJulianda <official.aryajulianda@gmail.com>
2023-09-04 08:06:09 +0700

    turn secure true

--- src/model/authModel.js
+++ src/model/authModel.js
@@ -55,6 +55,7 @@ exports.sendActivationEmail = (to, subject, content) => {
   const transporter = nodemailer.createTransport({
     host: 'smtp-relay.brevo.com',
     port: 587,
+    secure:true,
     auth: {
         user: 'arya.julianda21@gmail.com',
         pass: 'HMLGs20KDJpyNCbj'
